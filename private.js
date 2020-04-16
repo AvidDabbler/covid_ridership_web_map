2a01d5b28cde547ad7478f89fcc6acfa6badc361
AvidDabbler <walter.k.jenkins@gmail.com>
2020-04-16 14:50:49 -0500

    ignore folder

--- private.js
+++
@@ -1,7 +0,0 @@
-const agol = () => {
-    return {
-        ridership: 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/arcgis/rest/services/COVID19_Riderhsip_WFL1/FeatureServer',
-        heatmap:'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/arcgis/rest/services/COVID19_Riderhsip_heatmap/FeatureServer'}
-};
-
-export {agol}
