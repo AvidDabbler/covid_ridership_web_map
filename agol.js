acd4e6380132d6340b6f8c29ac3c63b4f2aa90da
AvidDabbler <walter.k.jenkins@gmail.com>
2020-04-28 18:11:41 -0500

    public

---
+++ agol.js
@@ -0,0 +1,8 @@
+const agol = () => {
+    return {
+        ridership: 'https://services2.arcgis.com/ZV8Mb62EedSw2aTU/arcgis/rest/services/COVID19_Riderhsip_WFL1/FeatureServer',
+        mapid: '48194abdb1d24415bb24c401f2e28cb8',
+       }
+};
+
+export {agol}
